3a0550de1de0f68d0c0cafffd9837ee4a9c6f90d
YenNhi200902 <nhiyen200802@gmail.com>
2024-06-15 08:03:18 +0700

    uplpad

---
+++ assets/js/slider.js
@@ -0,0 +1,120 @@
+let images = [
+  {
+    url: "https://image.nhandan.vn/Uploaded/2024/unqxwpejw/2023_09_24/anh-dep-giao-thong-1626.jpg",
+    caption: "404 wallpaper",
+    info: "Sample info",
+  },
+  {
+    url: "https://image.nhandan.vn/Uploaded/2024/unqxwpejw/2023_09_24/anh-dep-giao-thong-1626.jpg",
+    caption: "404 second wallpaper",
+    info: "Sample info",
+  },
+  {
+    url: "https://image.nhandan.vn/Uploaded/2024/unqxwpejw/2023_09_24/anh-dep-giao-thong-1626.jpg",
+    caption: "404 third wallpaper",
+    info: "Sample info",
+  },
+  {
+    url: "https://image.nhandan.vn/Uploaded/2024/unqxwpejw/2023_09_24/anh-dep-giao-thong-1626.jpg",
+    caption: "404 fourth wallpaper",
+    info: "Sample info",
+  },
+  {
+    url: "https://image.nhandan.vn/Uploaded/2024/unqxwpejw/2023_09_24/anh-dep-giao-thong-1626.jpg",
+    caption: "404 fifth wallpaper",
+    info: "Sample info",
+  },
+  {
+    url: "https://image.nhandan.vn/Uploaded/2024/unqxwpejw/2023_09_24/anh-dep-giao-thong-1626.jpg",
+    caption: "404 sixth wallpaper",
+    info: "Sample info",
+  },
+  {
+    url: "https://image.nhandan.vn/Uploaded/2024/unqxwpejw/2023_09_24/anh-dep-giao-thong-1626.jpg",
+    caption: "404 seventh wallpaper",
+    info: "Sample info",
+  },
+  {
+    url: "https://image.nhandan.vn/Uploaded/2024/unqxwpejw/2023_09_24/anh-dep-giao-thong-1626.jpg",
+    caption: "404 eighth wallpaper",
+    info: "Sample info",
+  },
+];
+const mainView = document.getElementById("main-view");
+const caption = document.getElementById("caption");
+const info = document.getElementById("info");
+
+const thumbnails = document.getElementById("thumbnails");
+
+for (let i = 0; i < images.length; i++) {
+  let image = images[i];
+  let img = document.createElement("img");
+  img.src = images[i].url;
+  img.setAttribute("width", 170);
+  img.setAttribute("data-index", i);
+  img.addEventListener("click", changeImage);
+  thumbnails.appendChild(img);
+}
+
+function initGallery() {
+  loadImage(0);
+}
+
+function slideImage() {
+  let currentIndex = parseInt(mainView.getAttribute("data-index"));
+  currentIndex = currentIndex + 1 == images.length ? 1 : currentIndex + 1;
+  loadImage(currentIndex);
+  setTimeout(slideImage, 3000);
+}
+
+function changeImage(event) {
+  let target = event.currentTarget;
+  let index = target.getAttribute("data-index");
+  loadImage(index);
+}
+
+function loadImage(index) {
+  let image = images[index];
+  mainView.src = image.url;
+  mainView.setAttribute("data-index", index);
+  mainView.setAttribute("id", "image-" + index);
+  mainView.style.opacity = 1;
+  caption.textContent = image.caption;
+  info.textContent = image.info;
+}
+
+function fullScreenImage() {
+  toggleFullscreen(mainView);
+}
+
+function toggleFullscreen(el) {
+  if (
+    document.fullscreenElement ||
+    document.mozFullScreenElement ||
+    document.webkitFullscreenElement ||
+    document.msFullscreenElement
+  ) {
+    if (document.exitFullscreen) {
+      document.exitFullscreen();
+    } else if (document.mozCancelFullScreen) {
+      document.mozCancelFullScreen();
+    } else if (document.webkitExitFullscreen) {
+      document.webkitExitFullscreen();
+    } else if (document.msExitFullscreen) {
+      document.msExitFullscreen();
+    }
+  } else {
+    if (document.documentElement.requestFullscreen) {
+      el.requestFullscreen();
+    } else if (document.documentElement.mozRequestFullScreen) {
+      el.mozRequestFullScreen();
+    } else if (document.documentElement.webkitRequestFullscreen) {
+      el.webkitRequestFullscreen();
+    } else if (document.documentElement.msRequestFullscreen) {
+      el.msRequestFullscreen();
+    }
+  }
+}
+
+initGallery();
+setTimeout(slideImage, 3000);
